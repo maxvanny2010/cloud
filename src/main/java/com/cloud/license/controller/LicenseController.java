cba5dbec3922107e0ec5917d5546ae0ab80104a1
maxvanny2010 <39569512+maxvanny2010@users.noreply.github.com>
2022-06-29 13:23:11 +0300

    RESTController

---
+++ src/main/java/com/cloud/license/controller/LicenseController.java
@@ -0,0 +1,16 @@
+package com.cloud.license.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * LicenseController.
+ *
+ * @author legion
+ * @version 5.0
+ * @since 29.06.2022
+ */
+@RestController
+@RequestMapping(value = "v1/organization/{organizationId}/license")
+public class LicenseController {
+}
