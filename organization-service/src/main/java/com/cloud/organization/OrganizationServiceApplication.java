ec9f31bc81f79a9e8df608a2cd9abb5d7b98ec66
maxvanny2010 <39569512+maxvanny2010@users.noreply.github.com>
2022-08-03 08:48:20 +0300

    pomade

--- organization-service/src/main/java/com/cloud/organization/OrganizationServiceApplication.java
+++ organization-service/src/main/java/com/cloud/organization/OrganizationServiceApplication.java
@@ -1,4 +1,4 @@
-package com.cloud.organizationservice;
+package com.cloud.organization;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
