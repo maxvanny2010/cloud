7e3e1d3e900216e289c0eefb06a752bf3255676c
maxvanny2010 <39569512+maxvanny2010@users.noreply.github.com>
2022-11-17 12:01:40 +0200

    Organization service

---
+++ licensing-service/src/main/java/com/cloud/license/service/OrganizationService.java
@@ -0,0 +1,14 @@
+package com.cloud.license.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * OrganizationService.
+ *
+ * @author legion
+ * @version 5.0
+ * @since 16.11.2022
+ */
+@Service
+public class OrganizationService {
+}
