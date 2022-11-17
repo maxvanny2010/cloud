86d31f94563a8df28cccb2736e946d5e3fc86d5b
maxvanny2010 <39569512+maxvanny2010@users.noreply.github.com>
2022-11-17 12:03:34 +0200

    LicenseServiceApplication Client Feign/Discovery/RestTemplate @Bean LoadBalancer

--- licensing-service/src/main/java/com/cloud/license/LicenseServiceApplication.java
+++ licensing-service/src/main/java/com/cloud/license/LicenseServiceApplication.java
@@ -2,19 +2,25 @@ package com.cloud.license;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import java.util.*;
 
 @SpringBootApplication
 @RefreshScope
-public class CloudApplication {
+@EnableDiscoveryClient
+@EnableFeignClients
+public class LicenseServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CloudApplication.class, args);
+        SpringApplication.run(LicenseServiceApplication.class, args);
     }
 
     @Bean
@@ -37,5 +43,10 @@ public class CloudApplication {
         //найти сообщение в файле по умолчанию, который называется messages.properties.
         messageSource.setBasenames("messages");
         return messageSource;
+    }
+    @LoadBalanced
+    @Bean
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
     }
 }
