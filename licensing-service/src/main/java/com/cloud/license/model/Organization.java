414b44011f9195e176dff3830026a9628b419d2c
maxvanny2010 <39569512+maxvanny2010@users.noreply.github.com>
2022-11-17 12:01:11 +0200

    Organization

---
+++ licensing-service/src/main/java/com/cloud/license/model/Organization.java
@@ -0,0 +1,20 @@
+package com.cloud.license.model;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.hateoas.RepresentationModel;
+
+@Getter
+@Setter
+@ToString
+public class Organization extends RepresentationModel<Organization> {
+
+    String id;
+    String name;
+    String contactName;
+    String contactEmail;
+    String contactPhone;
+
+}
