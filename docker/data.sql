22c9c50c1feff8a885849e8021b3835613693ac5
maxvanny2010 <39569512+maxvanny2010@users.noreply.github.com>
2022-12-25 20:01:06 +0200

    data.sql set data resource

---
+++ docker/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO public.organizations
+VALUES ('e6a625cc-718b-48c2-ac76-1dfdff9a531e', 'cloud', 'maxvanny', 'maxvanny2010', '888888888');
+INSERT INTO public.organizations
+VALUES ('d898a142-de44-466c-8c88-9ceb2c2429d3', 'cloud', 'Admin', 'maxvanny2010', '888888888');
+INSERT INTO public.organizations
+VALUES ('e839ee96-28de-4f67-bb79-870ca89743a0', 'cloud', 'maxvanny', 'maxvanny2010', '888888888');
+INSERT INTO public.licenses
+VALUES ('f2a9c9d4-d2c0-44fa-97fe-724d77173c62', 'd898a142-de44-466c-8c88-9ceb2c2429d3',
+        'Software Product', 'cloud', 'complete', 'I AM DEV');
+INSERT INTO public.licenses
+VALUES ('279709ff-e6d5-4a54-8b55-a5c37542025b', 'e839ee96-28de-4f67-bb79-870ca89743a0',
+        'Software Product', 'cloud', 'complete', 'I AM DEV');
