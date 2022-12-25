a11d864ee05a0d21e7a67eb7e4a8b4be062902ef
maxvanny2010 <39569512+maxvanny2010@users.noreply.github.com>
2022-12-25 20:14:43 +0200

    CircusBreaker/BulkHeads/FallBack/Retry/Rate Limiter

--- licensing-service/src/main/java/com/cloud/license/service/LicenseService.java
+++ licensing-service/src/main/java/com/cloud/license/service/LicenseService.java
@@ -7,9 +7,17 @@ import com.cloud.license.repository.LicenseRepository;
 import com.cloud.license.service.client.OrganizationDiscoveryClient;
 import com.cloud.license.service.client.OrganizationFeignClient;
 import com.cloud.license.service.client.OrganizationRestTemplateClient;
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.bulkhead.annotation.Bulkhead.Type;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
 import java.util.*;
+import java.util.concurrent.*;
 
 /**
  * LicenseService.
@@ -20,6 +28,7 @@ import java.util.*;
  */
 @Service
 public class LicenseService {
+    private static final Logger logger = LoggerFactory.getLogger(LicenseService.class);
     private final MessageSource messages;
 
     private final LicenseRepository licenseRepository;
@@ -51,7 +60,6 @@ public class LicenseService {
         if (null == license) {
             throw new IllegalArgumentException(String.format(messages.getMessage("license.search.error.message", null, null), licenseId, organizationId));
         }
-
         Organization organization = retrieveOrganizationInfo(organizationId, clientType);
         if (null != organization) {
             license.setOrganizationName(organization.getName());
@@ -101,12 +109,46 @@ public class LicenseService {
         License license = new License();
         license.setLicenseId(licenseId);
         licenseRepository.delete(license);
-        responseMessage = String.format(messages.getMessage("license.delete.message", null, null), licenseId);
+        responseMessage = String.format(
+                messages.getMessage("license.delete.message", null, null),
+                licenseId);
         return responseMessage;
 
     }
 
-    public List<License> getLicensesByOrganization(String organizationId) {
+    @CircuitBreaker(name = "license", fallbackMethod = "buildFallbackLicenseList")
+    @RateLimiter(name = "license", fallbackMethod = "buildFallbackLicenseList")
+    @Retry(name = "retryLicense", fallbackMethod = "buildFallbackLicenseList")
+    @Bulkhead(name = "bulkheadLicense", type = Type.THREADPOOL, fallbackMethod = "buildFallbackLicenseList")
+    public List<License> getLicensesByOrganization(String organizationId)
+            throws TimeoutException {
+        randomlyRunLong();
         return licenseRepository.findByOrganizationId(organizationId);
+    }
+
+    private List<License> buildFallbackLicenseList(String organizationId, Throwable t) {
+        List<License> fallbackList = new ArrayList<>();
+        License license = new License();
+        license.setLicenseId("0000000-00-00000");
+        license.setOrganizationId(organizationId);
+        license.setProductName("Sorry no licensing information currently available");
+        fallbackList.add(license);
+        return fallbackList;
+    }
+
+    private void randomlyRunLong() throws TimeoutException {
+        Random rand = new Random();
+        int randomNum = rand.nextInt((3 - 1) + 1) + 1;
+        if (randomNum == 3) sleep();
+    }
+
+    private void sleep() throws TimeoutException {
+        try {
+            System.out.println("Sleep");
+            Thread.sleep(5000);
+            throw new java.util.concurrent.TimeoutException();
+        } catch (InterruptedException e) {
+            logger.error(e.getMessage());
+        }
     }
 }
